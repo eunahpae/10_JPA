be521b8b7e237b33658a5dd2c682903f52f10c0e
eunahPae <eunahp86@gmail.com>
2025-06-17 11:30:28 +0900

    test : EntityManager 생성 및 스코프 테스트

--- chap01-persistance-context/src/test/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGeneratorTests.java
+++ chap01-persistance-context/src/test/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGeneratorTests.java
@@ -1,8 +1,10 @@
 package com.ohgiraffers.section01.entitymanager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,6 +45,28 @@ public class EntityManagerFactoryGeneratorTests {
         System.out.println("동일 인스턴스 여부: " + (factory1 == factory2));
     }
 
-    
+    @Test
+    @DisplayName("엔티티 매니저 생성 확인")
+    void testGeneratorEntityManager() {
+        // given
+        // when
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+
+        // then
+        assertNotNull(entityManager);
+    }
+
+    @Test
+    @DisplayName("엔티티 매니저 스코프 확인")
+    void testEntityManagerLifeCycle() {
+        // given
+        // when
+        EntityManager entityManager1 = EntityManagerGenerator.getInstance();
+        EntityManager entityManager2 = EntityManagerGenerator.getInstance();
+
+        // then
+        assertNotEquals(entityManager1, entityManager2);
+    }
+
 
 }
